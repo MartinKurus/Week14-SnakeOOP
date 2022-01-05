2c82a733e17d64939b8b2a95fcadd8096f4e2627
Opilane TTHK <opilane@tthk.ee>
2022-01-05 10:51:16 +0200

    first commit

---
+++ SnakeOOP1/Snake.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeOOP1
+{
+    class Snake : Figure
+    {
+        Direction direction;
+        public Snake(Point tail, int length, Direction _direction)
+        {
+            direction = _direction;
+            pointList = new List<Point>();
+            for(int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pointList.Add(p);
+            }
+
+        }
+        public void Move()
+        {
+            Point tail = pointList.First();
+            pointList.Remove(tail);
+            tail.Clear();
+
+            Point head = GetNextPoint();
+            pointList.Add(head);
+            head.Draw();
+
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pointList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+
+        }
+    }
+}
